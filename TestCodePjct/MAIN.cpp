c25780371e96ff0564f6f436296f86e60d970fa0
hirayasu <likedntzr@gmail.com>
2014-01-18 17:01:16 +0900

    17:01

--- TestCodePjct/MAIN.cpp
+++ TestCodePjct/MAIN.cpp
@@ -4,17 +4,29 @@
 
 using namespace std;
 
-TEST00 test00;
+TEST00 *test00;
+TEST01 *test01;
+void Constructor(){
+  test00 = new TEST00;
+  test01 = new TEST01;
+}
+void Destructor(){
+  delete test00;
+  delete test01;
+}
 
 int main() {
-
   cout << "Hello World !!" << endl;
+  Constructor();
 
   //test00.now_days();
   //test00.now_times();
 
-  test00.CopyCode();
+  test00->CopyCode();
+
+  test01->OutputFile();
 
   getchar();
+  Destructor();
   return 0;
 }
--- TestCodePjct/TEST01.cpp
+++ TestCodePjct/TEST01.cpp
@@ -1,13 +1,15 @@
 #include "TEST01.h"
 #include <stdio.h>
 
-TEST01::TEST01(){}
-TEST01::~TEST01(){}
+TEST01::TEST01(){
+}
+TEST01::~TEST01(){
+}
 
 void TEST01::OutputFile(){
   FILE* fp;
-  fopen_s(&fp, "FILE.file", "w");
-  fprintf_s(fp, "テキスト\n");
+  fopen_s(&fp, "TestFile//FILE.txt", "w");
+  fprintf_s(fp, "output test\n");
   fclose(fp);
 }
 void TEST01::InputFile(){
