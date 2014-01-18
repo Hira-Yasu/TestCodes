7c36e0887512a0b936d082e89ff53e3a8be9a324
hirayasu <likedntzr@gmail.com>
2014-01-18 15:22:27 +0900

    15：22

--- TestCodePjct/TEST00.cpp
+++ TestCodePjct/TEST00.cpp
@@ -8,34 +8,34 @@ TEST00::TEST00(){}
 TEST00::~TEST00(){}
 /*
 void TEST00::now_days(){
-  cout<<"日付を表示します。"<<endl;
+cout<<"日付を表示します。"<<endl;
 
-  time_t now = time(NULL);
-  struct tm *pnow = localtime_s(pnow, &now);//エラー
-  printf("%2d:%2d:%2d \n", pnow->tm_year+1900, pnow->tm_mon+1, pnow->tm_mday);
+time_t now = time(NULL);
+struct tm *pnow = localtime_s(pnow, &now);//エラー
+printf("%2d:%2d:%2d \n", pnow->tm_year+1900, pnow->tm_mon+1, pnow->tm_mday);
 
-  cout
-    <<pnow->tm_year+1900<<"年"
-    <<pnow->tm_mon+1<<"月"
-    <<pnow->tm_mday<<"日"<<endl;
+cout
+<<pnow->tm_year+1900<<"年"
+<<pnow->tm_mon+1<<"月"
+<<pnow->tm_mday<<"日"<<endl;
 
 }
 void TEST00::now_times(){
-  cout<<"\n現在時刻を表示します。"<<endl;
+cout<<"\n現在時刻を表示します。"<<endl;
 
-  time_t now = time(NULL);
-  struct tm *pnow = localtime_s(pnow, &now);//同じくエラー
-  printf("%d:%d:%d\n", pnow->tm_hour, pnow->tm_min, pnow->tm_sec);
+time_t now = time(NULL);
+struct tm *pnow = localtime_s(pnow, &now);//同じくエラー
+printf("%d:%d:%d\n", pnow->tm_hour, pnow->tm_min, pnow->tm_sec);
 
-  cout
-    <<pnow->tm_hour<<"時"
-    <<pnow->tm_min<<"分"
-    <<pnow->tm_sec<<"秒"<<endl;
+cout
+<<pnow->tm_hour<<"時"
+<<pnow->tm_min<<"分"
+<<pnow->tm_sec<<"秒"<<endl;
 
 }
 */
 void TEST00::CopyCode(){
-  
+
   //MicrosoftDNからコピペ
 
   struct tm newtime;
--- TestCodePjct/TEST01.cpp
+++ TestCodePjct/TEST01.cpp
@@ -4,11 +4,11 @@
 TEST01::TEST01(){}
 TEST01::~TEST01(){}
 
-  void TEST01::OutputFile(){
-    FILE* fp;
-    fopen_s(&fp, "FILE.file", "w");
-    fprintf_s(fp, "テキスト\n");
-    fclose(fp);
-  }
-  void TEST01::InputFile(){
-  }
+void TEST01::OutputFile(){
+  FILE* fp;
+  fopen_s(&fp, "FILE.file", "w");
+  fprintf_s(fp, "テキスト\n");
+  fclose(fp);
+}
+void TEST01::InputFile(){
+}
